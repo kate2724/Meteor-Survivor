2cb921a735f985b9f2222fcdba14b3b60d8ec973
kate2724 <kbreen@macalester.edu>
2021-01-04 20:47:23 -0600

    deleted files, and added readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-"# Meteor-Survivor" 
+"# Meteor-Survivor"
+Shoot the aliens before they take over the world!
--- music.py
+++
@@ -1,4 +0,0 @@
-from playsound import playsound
-
-# playsound('sawsquarenoise_-_10_-_Towel_Defence_Ending.mp3')
-playsound('sawsquarenoise_-_03_-_Towel_Defence_Ingame.mp3')
